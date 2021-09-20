43284e5c25a35515cf57387df14b8f480733da55
muby97 <mubarakfolorunsho@gmail.com>
2021-09-20 04:26:15 +0100

    Update Pricing.js

--- src/routes/Pricing/Pricing.js
+++ src/routes/Pricing/Pricing.js
@@ -32,38 +32,62 @@ export const Pricing = (props) => {
         align="center"
         backgound={SUB_BG}
         style={{
-          backgroundImage: `url(${SUB_BG})`,
+          // backgroundImage: `url(${SUB_BG})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
         }}
         minHeight="40vh"
       >
-        <Text padding="40px 5px 5px 5px" fontSize="20px" fontWeight="bold">
+        <Text padding="80px 5px 50px 5px" fontSize="30px" fontWeight="normal">
           Choose your subscription plan
         </Text>
-        <Text color={Theme.SecondaryTextColor}>Select your plan</Text>
       </Boxed>
       <Boxed
         width="100%"
-        pad="0 0 60px 0"
+        pad="20px 0 60px 0"
         height="100%"
         display="flex"
         position="relative"
       >
         <Boxed maxWidth="1080px" width="100%" margin="0 auto">
           <Boxed margin="-20vh 0 0 0">
-            <Grid desktop="50% 50%" tablet="40% 60%" mobile="100%">
-              <Boxed margin="0 0 0.5rem 0" display="flex" width="100%">
+            <Grid desktop="33% 33% 33%" tablet="50% 50%" mobile="100%">
+
+            {viewMode !== "mobile" && (
+            <Boxed margin="0 0 0.5rem 0" display="flex" width="100%">
                 <Boxed
-                  pad="40px 20px"
-                  margin="10px auto"
-                  bColor="#FFFFFF"
+                  pad="40px 20px 0px 40px"
+                  margin="10px 20px"
+                  bColor="#eb4149"
                   maxWidth="350px"
-                  boxShadow="-3px 3px 6px 4px #00000020"
+                  boxShadow="0 6px 20px rgba(56, 125, 255, 0.2)"
                   borderRadius="25px"
                 >
-                  <img src={STUDENTS} height="50px" alt="read-online" />
+                  <Text
+                    padding="40px 40px 10px 0px"
+                    fontSize="28px"
+                    fontWeight="bold"
+                    color={Theme.TertiaryDark}
+                  >
+                    A subscription Plan for Everyone
+                  </Text>
+                  <Text color={Theme.TertiaryDark}>
+                    Get Started
+                  </Text>
+                </Boxed>
+              </Boxed>)}
+            
+              <Boxed margin="0 0 0rem 0" display="flex" width="100%">
+                <Boxed
+                  pad="40px 40px 0px 40px"
+                  margin="10px 20px"
+                  bColor="#FFFFFF"
+                  maxWidth="350px"
+                  boxShadow="0 6px 20px rgba(56, 125, 255, 0.2)"
+                  borderRadius="25px"
+                >
+                  {/* <img src={STUDENTS} height="50px" alt="read-online" /> */}
                   <Text
                     padding="40px 0 10px 0"
                     fontSize="28px"
@@ -72,8 +96,7 @@ export const Pricing = (props) => {
                     {SubcriptionPlans.basic.plan}
                   </Text>
                   <Text color={Theme.SecondaryTextColor}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-                    blandit non id varius.
+                  The fundamental plan to get you started and increase your productivity as a legal Practitioner.
                   </Text>
                   <SubscribeList>
                     <li>
@@ -122,20 +145,20 @@ export const Pricing = (props) => {
                   </Button>
                 </Boxed>
               </Boxed>
-              <Boxed margin="0 0 0.5rem 0" display="flex" width="100%">
+              <Boxed margin="0 0 0rem 0" display="flex" width="100%">
                 <Boxed
-                  pad="40px 20px"
-                  margin="10px auto"
+                  pad="40px 40px 40px 40px"
+                  margin="10px 20px"
                   bColor="#FFFFFF"
                   maxWidth="350px"
-                  boxShadow="-3px 3px 6px 4px #00000020"
+                  boxShadow="0 6px 20px rgba(56, 125, 255, 0.2)"
                   borderRadius="25px"
                 >
-                  <img
+                  {/* <img
                     src={LEGAL_PRACTITIONER}
                     height="50px"
                     alt="read-online"
-                  />
+                  /> */}
                   <Text
                     padding="40px 0 10px 0"
                     fontSize="28px"
@@ -144,8 +167,7 @@ export const Pricing = (props) => {
                     {SubcriptionPlans.professional.plan}
                   </Text>
                   <Text color={Theme.SecondaryTextColor}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vel
-                    blandit non id varius.
+                  Get Access to Basic feature and all the new / latest features coming on AAER.
                   </Text>
                   <SubscribeList>
                     <li>
