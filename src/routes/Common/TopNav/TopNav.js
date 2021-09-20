6d8d093fb7620a1270f949dbdd22a3a56381d03d
muby97 <mubarakfolorunsho@gmail.com>
2021-09-20 02:49:03 +0100

    Update TopNav.js

--- src/routes/Common/TopNav/TopNav.js
+++ src/routes/Common/TopNav/TopNav.js
@@ -6,7 +6,7 @@ import { Button } from "../../../components/Button.components";
 
 import { calcViewMode } from "../../../utils/utils";
 
-import LOGO_FULL from "../../../assets/img/logo-full.png";
+import LOGO_FULL from "../../../assets/img/aaer-logo.png";
 import { HeaderNav } from "../../style";
 
 export const TopNav = (props) => {
@@ -19,7 +19,7 @@ export const TopNav = (props) => {
           desktop="repeat(3, 1fr)"
           tablet="repeat(3, 1fr)"
           mobile="120px auto"
-          pad="10px"
+          pad="10px 30px 10px 30px"
         >
           <Boxed>
             <img src={LOGO_FULL} alt="app_logo" height="40px" />
@@ -28,19 +28,20 @@ export const TopNav = (props) => {
             <Boxed display="flex" align="center">
               <HeaderNav>
                 <li onClick={() => redirect("/")}>Home</li>
-                {/* <li onClick={() => redirect("/features")}>Feature</li> */}
+                <li onClick={() => redirect("/features")}>Feature</li>
                 <li onClick={() => redirect("/pricing")}>Pricing</li>
               </HeaderNav>
             </Boxed>
           )}
           <Boxed align="right">
+          {viewMode !== "mobile" && (
             <Button
               margin="5px 0"
               pale
               onClick={() => redirect("/registration")}
             >
               Register
-            </Button>
+            </Button>)}
             <Button margin="5px 0" onClick={() => redirect("/login")}>
               Login
             </Button>
