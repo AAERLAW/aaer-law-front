6936ac4ac73d57ea558b56c214e6db1db485cd02
muby97 <mubarakfolorunsho@gmail.com>
2021-09-21 12:26:21 +0100

    Update TopNav.js

--- src/routes/Common/TopNav/TopNav.js
+++ src/routes/Common/TopNav/TopNav.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
 import { Grid } from "../../../components/Grid.components";
 import { Boxed } from "../../../components/Boxed.components";
 import { Button } from "../../../components/Button.components";
@@ -7,11 +6,20 @@ import { Button } from "../../../components/Button.components";
 import { calcViewMode } from "../../../utils/utils";
 
 import LOGO_FULL from "../../../assets/img/aaer-logo.png";
-import { HeaderNav } from "../../style";
+import { HeaderNav, MobileIcon, NavMenu } from "../../style";
+import { Hamburger } from "../../../assets/svg/hamburger.js";
+import { Times } from "../../../assets/svg/times.js";
 
 export const TopNav = (props) => {
   const { redirect } = props;
   const viewMode = calcViewMode();
+
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       <Boxed>
@@ -24,27 +32,35 @@ export const TopNav = (props) => {
           <Boxed>
             <img src={LOGO_FULL} alt="app_logo" height="40px" />
           </Boxed>
-          {viewMode !== "mobile" && (
+
+          {/* {viewMode !== "mobile" && ( */}
+          <NavMenu onClick={handleClick} click={click}>
             <Boxed display="flex" align="center">
               <HeaderNav>
                 <li onClick={() => redirect("/")}>Home</li>
-                <li onClick={() => redirect("/features")}>Feature</li>
+                <li onClick={() => redirect("/features")}>Features</li>
                 <li onClick={() => redirect("/pricing")}>Pricing</li>
               </HeaderNav>
             </Boxed>
-          )}
+          </NavMenu>
+          {/* )} */}
           <Boxed align="right">
-          {viewMode !== "mobile" && (
-            <Button
-              margin="5px 0"
-              pale
-              onClick={() => redirect("/registration")}
-            >
-              Register
-            </Button>)}
-            <Button margin="5px 0" onClick={() => redirect("/login")}>
+            {viewMode !== "mobile" && (
+              <Button
+                margin="5px 0"
+                pale
+                onClick={() => redirect("/registration")}
+              >
+                Register
+              </Button>)}
+            <Button margin="5px 15%" onClick={() => redirect("/login")}>
               Login
             </Button>
+            
+              <MobileIcon onClick={handleClick} align="right">
+                {click ? <Times /> : <Hamburger />}
+              </MobileIcon>
+            
           </Boxed>
         </Grid>
       </Boxed>
